9b3f4c2874d1fb94a8d4605282f70946468c38e7
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:52:59 +0530

    Add files via upload

---
+++ SingleInher.java
@@ -0,0 +1,39 @@
+package inheritance;
+class Furniture
+{
+	public void legs()
+	{
+		System.out.println("fur has 4 or 3 legs");
+	}
+	protected void seaters()
+	{
+		System.out.println("3 or 2 seaters");
+	}
+	private void types()
+	{
+		System.out.println("Plastic or wooden");
+	}
+}
+class Chair extends Furniture
+{
+	public void isFoldable()
+	{
+		System.out.println("yes");
+	}
+}
+
+public class SingleInher {
+
+	public static void main(String[] args) {
+		//Chair c = new Chair();
+				Furniture c=new Furniture();
+				c.seaters();
+				c.legs();
+				//c.types()//not accessible-private
+				//c.isFoldable();//not accessible by base class
+
+		// TODO Auto-generated method stub
+
+	}
+
+}
