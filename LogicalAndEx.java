58cbb3ba04aface21a57a232072861f70979c8f8
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:55:16 +0530

    Add files via upload

---
+++ LogicalAndEx.java
@@ -0,0 +1,19 @@
+package operators;
+
+public class LogicalAndEx {
+
+	public static void main(String[] args) {
+		  int x = 5;
+	        int y = 10;
+	        
+	        // Using logical AND operator to check if both x and y are greater than 0
+	        if (x > 0 && y > 0) 
+	        {
+	            System.out.println("Both x and y are greater than 0");
+	        } 
+	        else 
+	        {
+	            System.out.println(" not greater than 0");
+	        }
+}
+}
