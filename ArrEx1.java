4d0e4871baf7fa9ba72133411899a0493cd1e636
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:40:14 +0530

    Add files via upload

---
+++ ArrEx1.java
@@ -0,0 +1,36 @@
+package array;
+import java.util.*;
+
+
+public class ArrEx1 {
+
+	public static void main(String[] args) {
+		int[] marks=new int[5];
+        Scanner sc = new Scanner(System.in);
+        for(int i=0;i<marks.length;i++) //5<5
+        {
+       	 marks[i]=sc.nextInt();  //
+        }
+        for(int i=0;i<marks.length;i++)
+        {
+       	 System.out.println("Marks["+(i+1)+"]="+marks[i]);
+        }
+       /* int scores[]=new int[3];
+        scores[0]=10;
+        scores[1]=20;
+        scores[2]=30;
+        int age[]= {1,2,3};
+        int s[]=new int[] {1,4,8};*/
+     /*   Exam ob[]=new Exam[3];
+        ob[0]=new Exam();*/
+        
+	}
+
+}
+class Exam
+{
+	public Exam()
+	{
+		System.out.println("Cons called");
+	}
+}
