1950c6e7c94506bd624a6916b75bdcdf31def1d9
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:37:27 +0530

    Add files via upload

---
+++ ArraysEx.java
@@ -0,0 +1,24 @@
+package array;
+import java.util.*;
+
+
+public class ArraysEx {
+
+	public static void main(String[] args) {
+		//int arr[]= {11,30,15,7,90};
+				int arr[]= {20,11,98,34,22,76,42,10};
+				/*Arrays.sort(arr);
+				for(int a:arr)
+				{
+					System.out.print(a+" ");
+				}
+				System.out.println(Arrays.toString(arr));*/
+				Arrays.sort(arr,3,8);
+				for(int a:arr)
+				{
+					System.out.print(a+" ");
+				}
+				System.out.println(Arrays.toString(arr));
+			}
+
+		}
