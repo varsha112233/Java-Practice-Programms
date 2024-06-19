a97e6bd2ac86760cf8c4b70d983275c02b78e3a1
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:47:38 +0530

    Add files via upload

---
+++ DoWhileEx.java
@@ -0,0 +1,16 @@
+package constructs;
+
+public class DoWhileEx {
+
+	public static void main(String[] args) {
+
+     int a=5;
+     do
+     {
+    	 a++;
+    	 System.out.println(a);
+     }while(a<10);
+	}
+
+}
+		 
