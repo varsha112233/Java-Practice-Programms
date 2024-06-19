faabf31887fa9dd85785f303f24833a16a18fa23
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:49:37 +0530

    Add files via upload

---
+++ WhileEx.java
@@ -0,0 +1,15 @@
+package constructs;
+
+public class WhileEx {
+
+	public static void main(String[] args) {
+		  int a=5;
+		     while(a<10)
+		     {
+		    	 a++;
+		    	 System.out.println(a);
+		     }
+
+			}
+
+		}
