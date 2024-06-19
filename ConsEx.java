4ae663a59ea44a69d95621091745b277f670fcf1
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:50:37 +0530

    Add files via upload

---
+++ ConsEx.java
@@ -0,0 +1,14 @@
+package functions;
+
+public class ConsEx {
+
+		ConsEx(){
+			System.out.println("constructor  called");
+		}
+
+		public static void main(String[] args) {
+			// TODO Auto-generated method stub
+	       ConsEx ob=new ConsEx();
+		}
+
+	}
