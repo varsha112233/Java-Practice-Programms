2e619b343f9d48bd4962a893d06f589dd2766d49
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:54:59 +0530

    Add files via upload

---
+++ IncDecEx.java
@@ -0,0 +1,22 @@
+package operators;
+
+public class IncDecEx {
+
+	public static void main(String[] args) {
+		int i=5,j;
+		//i++;//i=i+1(post increment)
+		//++i;//i=i+1(pre increment)
+		//i--;//i=i-1(post)
+		//--i;//i=i-1(pre)
+		//System.out.println("i="+i);
+		//j=i++;//step 1:j=i  step 2:i=i+1
+		//System.out.println("i="+i+" j="+j);//i=6 j=5
+		//j=++i;//step 1:i=i+1 step 2: j=i
+		//System.out.println("i="+i+" j="+j); //i=6 j=6
+		//System.out.println(i++); //5
+		//System.out.println(i); //6
+		System.out.println(++i);  //6
+
+	}
+
+}
