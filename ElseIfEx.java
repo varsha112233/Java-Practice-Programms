e1c3cfd030cc78a5f87c821dbe929925cad1086d
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-19 16:47:54 +0530

    Add files via upload

---
+++ ElseIfEx.java
@@ -0,0 +1,24 @@
+package constructs;
+
+public class ElseIfEx {
+
+	public static void main(String[] args) {
+		int x = 1;
+
+		if (x > 10)
+		{
+		    System.out.println("x is greater than 10");
+		} 
+		else if (x < 10) 
+		{
+		    System.out.println("x is less than 10");
+		} 
+		else 
+		{
+		    System.out.println("x is equal to 10");
+		}
+
+
+	}
+
+}
