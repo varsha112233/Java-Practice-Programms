b144b208560ea1fd9f254146b07887fd4aa2e880
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-24 16:46:57 +0530

    Add files via upload

---
+++ Diffca1.java
@@ -0,0 +1,44 @@
+package collections;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+
+public class Diffca1 {
+
+	public static void main(String[] args) {
+		//Array
+		 /*   String[] a=new String[] {"w","e","l","c","o","m","e"};
+		    for(int i=0;i<a.length;i++)
+		    {
+		    	System.out.print(a[i]);
+		    }
+		    System.out.println();*/
+		    //collections
+		    ArrayList<String> al=new ArrayList<>();
+		    al.add("w");
+		    al.add("e");
+		    al.add("l");
+		    al.add("c");
+		    al.add("o");
+		    al.add("m");
+		    al.add("e");
+		    al.add(0,"hi ");
+		  //  System.out.println(al);
+		 //   System.out.println(al.get(4));
+		  /*  al.set(1,"to Java");
+		    System.out.print(al);*/
+		 /*  System.out.println(al.indexOf("e"));
+		   System.out.println(al.lastIndexOf("e"));*/
+		   // System.out.println(al);
+		  /*  al.remove(0);
+		    System.out.println(al);*/
+		  //  System.out.println(al.subList(4, 8));
+		   /* Iterator<String> itr=al.iterator();
+		    while(itr.hasNext())
+		    {
+		    	System.out.print(itr.next());
+		    }*/
+		    
+			}
+
+		}
