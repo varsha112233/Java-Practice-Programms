5a68f4084bc1293b585efd45324eb4cf02937bb8
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-06-26 16:23:53 +0530

    Add files via upload

---
+++ MapEx3.java
@@ -0,0 +1,25 @@
+package collections;
+import java.util.*;
+public class MapEx3 {
+        public static void main(String[] args) {
+            TreeMap<Integer, String> ageMap = new TreeMap<>();
+    		ageMap.put(25, "Alice");
+    		ageMap.put(35, "Charlie");
+    		ageMap.put(30, "Bob");
+    		ageMap.put(40, "David");
+    		/*System.out.println(ageMap.lowerKey(33)); // Returns the greatest key less than 33 (30)
+    		System.out.println(ageMap.higherKey(33)); // Returns the smallest key greater than 33		(35)
+    		System.out.println(ageMap.floorKey(37)); // Returns the greatest key less than or equal	to 37 (35)
+    		System.out.println(ageMap.ceilingKey(37)); // Returns the smallest key greater than or equal to 37 (40)
+    */
+    		/*int low=ageMap.lowerKey(33);
+    		System.out.println(ageMap.get(low));*/
+    		System.out.println(ageMap.firstKey());
+    		System.out.println(ageMap.lastKey());
+    		System.out.println(ageMap.descendingKeySet());
+    		System.out.println(ageMap.descendingMap());
+    	}
+
+    }
+  
+
