457472a86d88d050c33a5eebd400bc23ea9f5af6
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-07-03 21:21:46 +0530

    Add files via upload

---
+++ RaceConditionExample.java
@@ -0,0 +1,29 @@
+package thread;
+public class RaceConditionExample {
+    private static int counter = 0;
+       public static void main(String[] args) throws InterruptedException {
+    	        // Create two threads that increment the counter concurrently
+    	        Thread thread1 = new Thread(() -> {
+    	            for (int i = 0; i < 1000; i++) {
+    	                counter++;
+    	            }
+    	        });
+
+    	        Thread thread2 = new Thread(() -> {
+    	            for (int i = 0; i < 1000; i++) {
+    	                counter++;
+    	            }
+    	        });
+
+    	        // Start both threads
+    	        thread1.start();
+    	        thread2.start();
+
+    	        // Wait for both threads to complete
+    	        thread1.join();
+    	        thread2.join();
+
+    	        // Print the final value of counter
+    	        System.out.println("Final counter value: " + counter);
+    	    }
+    	}
