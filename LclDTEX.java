5ffe66b6c0cb42d564841a205f282bac02995648
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-07-15 19:16:07 +0530

    Add files via upload

---
+++ LclDTEX.java
@@ -0,0 +1,15 @@
+package localization;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+public class LclDTEX {
+        public static void main(String[] args) {
+        	LocalDate today=LocalDate.now();
+    		System.out.println("Today's date is :"+today);
+    		LocalDate oneWeek=today.plusWeeks(1);
+    		System.out.println("Next week :"+oneWeek);
+    		DateTimeFormatter dtf=DateTimeFormatter.ofPattern("dd-MMM-yyy");
+    		String res=today.format(dtf);
+    		System.out.println(res);
+    	}
+
+    }
