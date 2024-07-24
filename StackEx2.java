53585624370eb4d3ba999ea5a4756feb6bf96b7e
varsha112233 <118154147+varsha112233@users.noreply.github.com>
2024-07-24 16:03:35 +0530

    Add files via upload

---
+++ StackEx2.java
@@ -0,0 +1,30 @@
+package datastructures;
+import java.util.*;
+public class StackEx2 {
+	public static String reverseString(String input) 
+	{
+		Stack<Character> stack = new Stack<>();
+		// Push each character from the input string onto the stack
+		for (char c : input.toCharArray()) 
+		{
+		stack.push(c);
+		}
+		// Pop characters from the stack and append them to a new string
+		StringBuilder reversedString = new StringBuilder();
+		while (!stack.isEmpty()) 
+		{
+		reversedString.append(stack.pop());
+		}
+		return reversedString.toString();
+	}
+
+        public static void main(String[] args) {
+        	String input = "Hello, World!";
+        	String reversed = reverseString(input);
+        	System.out.println("Original string: " + input);
+        	System.out.println("Reversed string: " + reversed);	
+        	}
+
+        }
+        
+		
